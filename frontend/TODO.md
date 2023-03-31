7d83b0d0dc7f937591bb77739d81d281a5073ffe
JurajSternVukotic <jsternvuk@unipu.hr>
2023-03-31 22:05:47 +0200

    add language switching

--- frontend/TODO.md
+++ frontend/TODO.md
@@ -4,6 +4,8 @@
     - Make icon for page
     - Mage logo for Navbar
     - Make Icon for theme switcher
+    - Anim of icons being forged into text when hovered
+    - Page load navbar animation
 ---
 ## Themes
     - Modify theme colors when mockup pages are done
---
+++ frontend/javascript/translations.js
@@ -0,0 +1,24 @@
+const translations = {
+    en: {
+        home: "Home",
+        myWikis: "My Wikis",
+        friends: "Friends",
+        bookmarks: "Bookmarks",
+        anim: "ANIM",
+        tutorial: "Tutorial",
+        profile: "Profile",
+        language_toggler: "Language",
+        theme_toggler: "Theme",
+    },
+    hr: {
+        home: "Početna stranica",
+        myWikis: "Moji Wiki-i",
+        friends: "Prijatelji",
+        bookmarks: "Bilješke",
+        anim: "ANIM",
+        tutorial: "Upute",
+        profile: "Profil",
+        language_toggler: "Jezik",
+        theme_toggler: "Tema",
+    },
+};
